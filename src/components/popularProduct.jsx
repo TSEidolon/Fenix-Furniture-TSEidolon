4aa31612a289371f606d6926a6f0b65dd25a8089
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-11-05 05:42:10 -0700

    added styling

--- src/components/popularProduct.jsx
+++ src/components/popularProduct.jsx
@@ -1,36 +1,45 @@
 import React from 'react'
 import { products } from '../products'
+import { Link } from 'react-router-dom';
+
 
 export default function PopularProduct() {
+
   const firstProduct = products[3];
   const secondProduct = products[4];
 
   return (
-    <div>
+    <div className='flex items-center justify-center flex-col gap-5 max-w-full'>
       <h2> Most Pupular Items </h2>
       <div className='popular-product-main-content'>
-        <div className='top-content flex justify-center items-center'>
+        <div className='top-content flex justify-center items-center gap-10'>
           <div>
-            <h3>{firstProduct.name}</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate vel corporis, dicta obcaecati vitae et dolorem laboriosam nostrum molestiae eaque.</p>
-            <button>
-              Add to Cart
-            </button>
+            <h3 className='text-2xl py-5'>{firstProduct.name}</h3>
+            <p className='max-w-[600px] text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate vel corporis, dicta obcaecati vitae et dolorem laboriosam nostrum molestiae eaque.</p>
+            <Link to={firstProduct.slug} className='text-lg font-semibold pt-5'>
+              <button>
+                Read More
+              </button>
+            </Link>
+            
           </div>
           <div>
-            <img src={firstProduct.image} alt="" className='h-80 ' />
+            <img src={firstProduct.image} alt="" className='h-[26rem] ' />
           </div>
         </div>
-        <div className='bottom-content flex flex-row-reverse justify-center items-center'>
-          <div className='max-w-full'>
+        <div className='bottom-content flex flex-row-reverse justify-center items-center gap-10'>
+          <div className=''>
             <h3 className='text-2xl py-5'>{secondProduct.name}</h3>
-            <p className='max-w-[500px] text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quia dicta eos saepe obcaecati numquam culpa aperiam quis, sapiente mollitia nam, sit velit, eveniet quam fugit repellendus deserunt repellat? Cum!</p>
-            <button className='text-lg font-semibold pt-5'>
-              Add to Cart
-            </button>
+            <p className='max-w-[600px] text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quia dicta eos saepe obcaecati numquam culpa aperiam quis, sapiente mollitia nam, sit velit, eveniet quam fugit repellendus deserunt repellat? Cum!</p>
+            <Link to={secondProduct.slug}>
+              <button className='text-lg font-semibold pt-5'>
+                Read More
+              </button>
+            </Link>
+
           </div>
           <div>
-            <img src={secondProduct.image} alt="" className='h-80 ' />
+            <img src={secondProduct.image} alt="" className='h-[26rem] ' />
           </div>
         </div>
       </div>
