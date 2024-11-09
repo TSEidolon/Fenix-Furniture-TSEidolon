730b988c8c539ba154eb812d23f06e3bd3de98d0
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-11-08 19:02:15 -0700

    edited home page

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,7 +1,64 @@
 import React from 'react'
+import { CiDeliveryTruck } from "react-icons/ci";
+import { CiDiscount1 } from "react-icons/ci";
+import { PiChatCircleDotsLight } from "react-icons/pi";
 
 export default function HeroSection() {
   return (
-    <div>HeroSection</div>
+    <div className='pt-5 bg-[#f5f5f5]'>
+      <section className='hero-section flex items-center px-10 gap-5'>
+        <div className="hero-section-left flex flex-col gap-10">
+          <h1 className='text-6xl '>Super CatchPhrase Here!!!!!!!!!!</h1>
+          <p className='max-w-[70%]'> Mini Paragraph section description thingy here.... Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, fuga.</p>
+          <div className='flex gap-10'>
+            <button className='bg-[#deecdd] rounded font-medium py-1 px-2'> Shop Now </button>
+            <button className='bg-[#deecdd] rounded font-medium py-1 px-2'> Read More </button>
+          </div>
+        </div>
+        <div className="hero-section-right">
+          <div className='hero-section-right-background '>
+            <div className='bg-[#deecdd] rounded  w-[750px] h-[450px]' >
+              <p className=' flex justify-center items-center h-full'> Picture </p>
+            </div>
+          </div>
+          
+        </div>
+      </section>
+      <section className='flex justify-between items-center gap-10 px-10 pt-10'>
+        <div className='flex  items-center gap-10 border-2 border-black rounded min-w-[300px]'>
+          <div className="hero-banner-left border-r-2 border-r-black p-3 ">
+            <CiDeliveryTruck className='h-[70px] w-[70px]' />
+          </div>
+          <div className="hero-banner-right pr-3">
+            <h2 className='font-bold'>Free Shipping</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className='flex  items-center gap-10 border-2 border-black rounded min-w-[350px]'>
+          <div className="hero-banner-left border-r-2 border-r-black p-3 ">
+            <CiDiscount1 className='h-[70px] w-[70px]' />
+          </div>
+          <div className="hero-banner-right pr-3">
+            <h2 className='font-bold'>Free Shipping</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className='flex  items-center gap-10 border-2 border-black rounded min-w-[350px]'>
+          <div className="hero-banner-left border-r-2 border-r-black p-3 ">
+            <PiChatCircleDotsLight className='h-[70px] w-[70px]' />
+          </div>
+          <div className="hero-banner-right pr-3">
+            <h2 className='font-bold'>Free Shipping</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        
+      </section>
+      <section>
+        <p className='text-xl p-20'> 
+          Test Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem, cumque natus labore architecto voluptatibus iste molestiae beatae vitae temporibus omnis accusantium provident nihil quia dolore quas. Animi officiis earum quas nostrum facilis soluta laboriosam exercitationem tenetur dignissimos voluptatibus possimus officia ex voluptatum fugiat cum eius fuga mollitia labore, vero beatae! Dicta labore et vitae quam ipsa cum, consequuntur nihil facilis voluptas doloribus adipisci autem alias quas perspiciatis, accusantium molestias est ex quidem, hic soluta! Recusandae, mollitia culpa! Obcaecati aut, nesciunt praesentium omnis vitae laudantium accusamus magni quam repellat dolores vero ducimus voluptate alias itaque ipsa amet eveniet placeat odio? Veniam odio unde, cupiditate aut molestias nobis quasi officia sunt nesciunt, facere cumque quia delectus pariatur expedita modi quos, nemo aliquam tempore sequi laboriosam ratione perspiciatis laborum! Nemo modi similique praesentium et fugiat omnis quo illum non. Accusamus quam porro quas quasi, nesciunt natus blanditiis unde repudiandae explicabo sint vel ad ullam itaque, doloremque numquam. Sunt ex hic enim officia voluptatem consequatur tempore at neque minima omnis quasi sint quidem autem, repudiandae sapiente corrupti mollitia sed architecto. In cum, eveniet ipsum ex enim praesentium itaque, eos inventore unde sequi nulla? Eaque deleniti ratione eos obcaecati? Esse nobis ea dolores rerum veniam delectus. Culpa reiciendis labore recusandae pariatur molestias ratione iure, molestiae autem fugit obcaecati numquam itaque! Tempore odio exercitationem sapiente ullam similique? Libero officiis ex sint minus autem aliquid, alias enim ea unde ut. Fugiat voluptates placeat repellat ipsa in sapiente voluptatibus ad? Culpa aperiam earum assumenda architecto, cum recusandae repellendus mollitia corporis tenetur unde, excepturi maiores nobis quas repudiandae minima harum, quo odio accusamus aut! Sapiente at alias ipsam dolore! Obcaecati dolor facilis fuga recusandae laudantium omnis asperiores libero. Asperiores dicta tempora cum velit totam illum. Harum laborum eligendi nostrum cum necessitatibus voluptate sit explicabo earum laudantium est autem, ab tenetur hic nulla deleniti totam aperiam, unde doloremque, porro quibusdam quasi nobis non! Accusamus cumque magnam omnis ipsum in reiciendis natus voluptatum repellat obcaecati accusantium dolores repudiandae mollitia sit atque magni, voluptate consequuntur labore ea quaerat ullam iusto, culpa quas incidunt perspiciatis. Non, eveniet assumenda hic magnam deserunt saepe nam labore suscipit fugit quia sint veritatis, dicta veniam adipisci sit vel quaerat dolor eius quos unde nisi porro libero quae quam? Quae sed aspernatur nemo iusto sequi perspiciatis neque ex, cupiditate repellat tenetur architecto nisi dolores modi eos dolor illo aliquid ratione fuga quibusdam rem sit nihil deserunt. Reprehenderit itaque aspernatur vitae saepe explicabo nisi a, animi ab labore aut inventore blanditiis sed, doloremque voluptatum, placeat perferendis repellat distinctio impedit rerum nihil cumque quae velit? Totam possimus qui, necessitatibus accusamus sed, consectetur vitae ipsam laudantium eius sequi maiores, rerum natus. Velit quia dolores impedit assumenda et enim voluptatum in, tempora laboriosam quaerat eaque culpa doloremque tenetur aliquid doloribus iusto, ullam molestiae ratione amet ipsum dignissimos? Distinctio provident temporibus repellat quis nam quae placeat, consectetur necessitatibus molestias, laboriosam quod, accusantium maiores veritatis aliquid suscipit excepturi. Quia hic sit odio? Dolor necessitatibus, iste obcaecati nisi fugit dolorem provident recusandae tempora odit reiciendis?
+        </p>
+      </section>
+    </div>
   )
 }
