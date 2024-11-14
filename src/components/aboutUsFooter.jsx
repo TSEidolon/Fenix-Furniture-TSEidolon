3a77d46399bab60ddb7095963f12dcea14ddc7bd
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-11-14 11:01:45 -0700

    edited styles

--- src/components/aboutUsFooter.jsx
+++ src/components/aboutUsFooter.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { FaFacebook } from "react-icons/fa";
 
 export default function AboutUsFooter() {
   return (
     <div className='h-[90vh] pt-10'>
-        <section className='about-us-section flex items-center justify-between gap-10 pb-5 h-[60%]'>
+        <section className='about-us-section flex items-center justify-between gap-10 pb-10 h-[60%]'>
           <div className="about-us-section-left">
             <div className='hero-section-right-background '>
               <div className='bg-[#deecdd] rounded-full  w-[550px] h-[450px]' >
@@ -22,30 +23,40 @@ export default function AboutUsFooter() {
             </div>
           </div>
         </section>
-        <section className='footer flex justify-around items-start pt-5 gap-5 border-2 border-black h-[40%]'>
-          <div className='flex flex-col gap-3'>
+        <section className='footer flex justify-around items-start pt-5 gap-5 bg-[#C1DFC4] rounded-md h-[40%]'>
+          <div className='flex flex-col gap-3 items-start'>
             <h4 className='text-xl font-semibold'>Other Info</h4>
-            <p className='text-sm'>Terms & Conditions</p>
-            <p className='text-sm'>Privacy Policy</p>
-            <p className='text-sm'>License</p>
+            <button className='text-sm'>Terms & Conditions</button>
+            <button className='text-sm'>Privacy Policy</button>
+            <button className='text-sm'>License</button>
+          </div>
+          <div className='flex flex-col gap-3 items-start'>
+            <h4 className='text-xl font-semibold'>Media</h4>
+            <button className='text-sm'>Blogs</button>
+            <button className='text-sm'>Photo Gallery</button>
+            <button className='text-sm'>Latest Release</button>
+          </div>
+          <div className='flex flex-col gap-3 items-start'>
+            <h4 className='text-xl font-semibold'>Company</h4>
+            <button className='text-sm'>About Us</button>
+            <button className='text-sm'>Career</button>
+            <button className='text-sm'>Services</button>
           </div>
           <div className='flex flex-col gap-3'>
-            <h4 className='text-xl font-semibold'>Other Info</h4>
-            <p className='text-sm'>Terms & Conditions</p>
-            <p className='text-sm'>Privacy Policy</p>
-            <p className='text-sm'>License</p>
-          </div>
-          <div className='flex flex-col gap-3'>
-            <h4 className='text-xl font-semibold'>Other Info</h4>
-            <p className='text-sm'>Terms & Conditions</p>
-            <p className='text-sm'>Privacy Policy</p>
-            <p className='text-sm'>License</p>
-          </div>
-          <div className='flex flex-col gap-3'>
-            <h4 className='text-xl font-semibold'>Other Info</h4>
-            <p className='text-sm'>Terms & Conditions</p>
-            <p className='text-sm'>Privacy Policy</p>
-            <p className='text-sm'>License</p>
+            <h4 className='text-xl font-semibold'>Socials</h4>
+              <button className='flex items-center gap-2'>
+                <p className='text-sm'>Facebook</p>
+                <FaFacebook className='text-sm'/>
+              </button> 
+              <button className='flex items-center gap-2'>
+                <p className='text-sm'>X / Twitter</p>
+                <FaFacebook className='text-sm'/>
+              </button> 
+              <button className='flex items-center gap-2'>
+                <p className='text-sm'>Instagram</p>
+                <FaFacebook className='text-sm'/>
+              </button> 
+
           </div>
         </section>
 
